3c699613f6df7bc90ca1078ad25ee0e876fbc62f
Aaaliang <152960807+ALBULAING@users.noreply.github.com>
2024-02-26 19:32:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '阿凉不凉', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '随性拖延自由派🌊', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'miftahfar.eu.org', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '资源分享, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'aliang@miftahfar.eu.org', // 邮箱地址 例如mail@tangly1024.com
