7312a08980743b072aee72219fd879cd02ebd137
Aaaliang <152960807+ALBULAING@users.noreply.github.com>
2025-01-19 21:20:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -46,7 +46,7 @@ const BLOG = {
   ...require('./conf/dev.config'), // 开发、调试时需要关注的配置
 
   // 自定义外部脚本，外部样式
-  CUSTOM_EXTERNAL_JS: ['https://fastly.jsdelivr.net/gh/ALBULAING/StaticHub@1.0.0/tawk.to/tawk.js'], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
+  CUSTOM_EXTERNAL_JS: ['https://embed.tawk.to/6789c34e825083258e06a3cb/1ihp25rv2'], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
   CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
 
   // 自定义菜单
